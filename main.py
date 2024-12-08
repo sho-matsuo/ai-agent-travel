180fe431cf388048d293b90f27590945d1cc20ed
Yoshi <yooooki@gmail.com>
2024-12-08 15:26:54 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -379,25 +379,25 @@ def main():
 
 
     #データベース接続
-    # config = {
-    # 'user': 'aiagent-admin',         # MySQLユーザー名
-    # 'password': 'A!agent01',     # MySQLパスワード
-    # 'host': 'sql-server-aiagents.database.windows.net',             # ホスト名（例: 'localhost'）
-    # 'database': 'db_aiagents' # データベース名
-    # }
+    config = {
+    'user': 'aiagent-admin',         # MySQLユーザー名
+    'password': 'A!agent01',     # MySQLパスワード
+    'host': 'sql-server-aiagents.database.windows.net',             # ホスト名（例: 'localhost'）
+    'database': 'db_aiagents' # データベース名
+    }
 
-    # connection = mysql.connector.connect(**config)
-    # cursor = connection.cursor()
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
 
-    # # INSERT文の準備
-    # insert_query = "INSERT INTO input (input_data) VALUES (%s)"
-    # data_to_insert = (args,)  # 挿入するデータ（タプル形式）
+    # INSERT文の準備
+    insert_query = "INSERT INTO input (input_data) VALUES (%s)"
+    data_to_insert = (args,)  # 挿入するデータ（タプル形式）
 
-    # # クエリの実行
-    # cursor.execute(insert_query, data_to_insert)
+    # クエリの実行
+    cursor.execute(insert_query, data_to_insert)
 
-    # # トランザクションをコミット
-    # connection.commit()
+    # トランザクションをコミット
+    connection.commit()
 
     st.text("データが正常に挿入されました！")
 
