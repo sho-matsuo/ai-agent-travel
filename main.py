46c40133fdf33db592cf2f36125967a6f8ee3d3e
Yoshi <yooooki@gmail.com>
2024-12-08 17:38:29 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
-import streamlit as st
-import mysql.connector
+import pyodbc
+db_server = 'sql-server-aiagents.database.windows.net'
+database = 'db_aiagents'
+username = 'aiagent-admin'
+password = 'A!agent01'
+driver = '{ODBC Driver 17 for SQL Server}'
 
-args = st.text_input("作成したいアプリケーションについて記載してください")
-
-#データベース接続
-config = {
-'user': 'aiagent-admin',         # MySQLユーザー名
-'password': 'A!agent01',     # MySQLパスワード
-'host': 'sql-server-aiagents.database.windows.net',             # ホスト名（例: 'localhost'）
-'database': 'db_aiagents' # データベース名
-}
-
-connection = mysql.connector.connect(**config)
-cursor = connection.cursor()
-
-# INSERT文の準備
-insert_query = "INSERT INTO input (input_data) VALUES (%s)"
-data_to_insert = (args,)  # 挿入するデータ（タプル形式）
-
-# クエリの実行
-cursor.execute(insert_query, data_to_insert)
-
-# トランザクションをコミット
-connection.commit()
-
-st.text("データが正常に挿入されました！")
+with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+db_server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+password) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute('''
+                        CREATE TABLE test1(
+                            id int primary key,
+                            name nvarchar(50),
+                            price int
+                        )
+        ''')
+        consor.commit()
