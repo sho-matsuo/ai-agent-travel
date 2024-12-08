73e80426acf60d241323b2929910771753d41d6f
Yoshi <yooooki@gmail.com>
2024-12-08 15:44:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -377,31 +377,6 @@ def main():
     # args = parser.parse_args(["--task", "旅行アプリを開発したい"]) #sample code for simulation
     args = st.text_input("作成したいアプリケーションについて記載してください")
 
-
-    # #データベース接続
-    # config = {
-    # 'user': 'aiagent-admin',         # MySQLユーザー名
-    # 'password': 'A!agent01',     # MySQLパスワード
-    # 'host': 'sql-server-aiagents.database.windows.net',             # ホスト名（例: 'localhost'）
-    # 'database': 'db_aiagents' # データベース名
-    # }
-
-    # connection = mysql.connector.connect(**config)
-    # cursor = connection.cursor()
-
-    # # INSERT文の準備
-    # insert_query = "INSERT INTO input (input_data) VALUES (%s)"
-    # data_to_insert = (args,)  # 挿入するデータ（タプル形式）
-
-    # # クエリの実行
-    # cursor.execute(insert_query, data_to_insert)
-
-    # # トランザクションをコミット
-    # connection.commit()
-
-    st.text("データが正常に挿入されました！")
-
-
     # ChatOpenAIモデルを初期化
     llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.0)
     # 要件定義書生成AIエージェントを初期化
